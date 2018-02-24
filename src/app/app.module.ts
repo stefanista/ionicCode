a8294d08e5d3440c81008a6025574d6db80b2f28
Stephanie Retzke <smrhfb@mail.umkc.edu>
2018-02-23 23:48:44 -0600

    testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,59 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { LoggedinPage } from '../pages/loggedin/loggedin';
+import { RegisterPage } from '../pages/register/register';
+
+import { TabsPage } from '../pages/tabs/tabs';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
+const firebaseAuth = {
+    apiKey: "AIzaSyBjyQenurH7RFfj9iOC34USGn6BWgdG6AI",
+    authDomain: "login-project-36e3f.firebaseapp.com",
+    databaseURL: "https://login-project-36e3f.firebaseio.com",
+    projectId: "login-project-36e3f",
+    storageBucket: "login-project-36e3f.appspot.com",
+    messagingSenderId: "596843832188"
+  };
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    LoggedinPage,
+    AboutPage,
+    ContactPage,
+    TabsPage
+    
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseAuth),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    LoggedinPage,
+    AboutPage,
+    ContactPage,
+    TabsPage
+    
   ],
   providers: [
     StatusBar,
